3c56fee032d5fe902a180f0db03f1b36b8d7537f
Ed Summers <ehs@pobox.com>
2013-08-22 10:36:17 +0000

    handle pdfs that are not available yet

--- examples/pdf.py
+++ examples/pdf.py
@@ -13,7 +13,7 @@ import requests
 articles = json.loads(open('../metadata.json').read())
 for a in articles:
     slug = a["title"].lower()
-    slug = re.sub("/[^a-zA-Z0-9\s]/", "", slug)
+    slug = re.sub("[^a-zA-Z0-9\s]", "", slug)
     slug = slug.replace(" ", "-")
 
     pdf_filename = "pdf/%s/%s/%s.pdf" % (a["volume"], a["issue"], slug)
@@ -26,10 +26,14 @@ for a in articles:
     if not os.path.isdir(dir_name):
         os.makedirs(dir_name)
 
+    # last three volumes aren't available for free so don't store paywall html
     resp = requests.get(a["pdf"])
-    print a["pdf"], resp.status_code
+    print resp.headers["content-type"]
+    if resp.headers["content-type"] != "application/pdf":
+        continue
 
-    #open(json_filename, "w").write(json.dumps(a, indent=2))
-    #print pdf_filename
+    open(pdf_filename, "wb").write(resp.content)
+    open(json_filename, "w").write(json.dumps(a, indent=2))
+    print pdf_filename
     time.sleep(5)
 
